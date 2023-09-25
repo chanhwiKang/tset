cb9782f90139aab544da88473f157ca8d6844a8d
chanhwiKang <cksgnl4545@gmail.com>
2023-09-25 11:29:02 +0900

    err2

--- go/week05/main.go
+++ go/week05/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 )
 
 func main() {
 	fmt.Print("Enter a grade: ")
 	reader := bufio.NewReader(os.Stdin)
-	input := reader.ReadString('\n')
+	input, err := reader.ReadString('\n')
+	log.Fatal(err)
 	fmt.Println(input)
 }
