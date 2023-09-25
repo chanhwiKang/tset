64698814af0cf4d572e04fb949a964ec6d18308a
chanhwiKang <cksgnl4545@gmail.com>
2023-09-25 12:11:06 +0900

    dice0

--- go/week05/main.go
+++ go/week05/main.go
@@ -1,33 +1,11 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"log"
-	"os"
-	"strconv"
-	"strings"
+	"math/rand"
 )
 
 func main() {
-	fmt.Print("Enter a grade: ")
-	reader := bufio.NewReader(os.Stdin)
-	input, err := reader.ReadString('\n')
-	if err != nil {
-		log.Fatal(err)
-	}
-	input = strings.TrimSpace(input)
-	grade, err := strconv.ParseInt(input, 10, 32)
-	if err != nil {
-		log.Fatal(err)
-	}
-	var score string
-	if grade >= 90 {
-		score = "a grade"
-	} else {
-		score = "under A grade"
-	}
-	fmt.Println(input)
-	fmt.Println("You will get", score) // undefined: grade
-
+	dice := rand.Intn(6) + 1
+	fmt.Println(dice)
 }
