66fb8a0f1dfd2b8c09beaa4bb99ff4f706a53c57
chanhwiKang <cksgnl4545@gmail.com>
2023-09-25 10:48:38 +0900

    1

--- go/week05/main.go
+++ go/week05/main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"strings"
 )
 
 func main() {
-	var now time.Time = time.Now()
-	var year = now.Year()
-	fmt.Println(year)
+	broken := "G@ r@cks!"
+	replacer := strings.NewReplacer("@", "o")
+	fixed := replacer.Replace(broken)
+	fmt.Println(fixed)
 }
