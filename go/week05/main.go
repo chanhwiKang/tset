8fd98bc36672fe5c89344561e087b786d2452268
chanhwiKang <cksgnl4545@gmail.com>
2023-09-25 11:34:56 +0900

    err_clear_whth_if

--- go/week05/main.go
+++ go/week05/main.go
@@ -11,6 +11,9 @@ func main() {
 	fmt.Print("Enter a grade: ")
 	reader := bufio.NewReader(os.Stdin)
 	input, err := reader.ReadString('\n')
-	log.Fatal(err)
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println(input)
+
 }
